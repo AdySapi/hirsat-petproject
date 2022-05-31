e421cdbd6410509e2fc3ef9ac9d08e6fccd1ecb3
Your Name <you@example.com>
2022-05-31 11:02:37 +0200

    footer header tweeked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div>
+            <img src="https://user-images.githubusercontent.com/97350067/171006678-058f4e5e-db68-4623-91b7-92b574e11c65.jpeg" alt="" />
+            <h5>gofund.me/635cec61</h5>
+        </div>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,14 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <Link to='/team'>Team</Link>
-            <Link to='/gallery'>Gallery</Link>
-            <Link to='/achievements'>Achievements</Link>
-            <Link to='/contact'>Contact</Link>
+            <img src="https://user-images.githubusercontent.com/97350067/171006678-058f4e5e-db68-4623-91b7-92b574e11c65.jpeg" alt="" />
+            <div>
+                <h1>Hír-Sat BHC</h1>
+                <Link to='/team'>Team</Link>
+                <Link to='/gallery'>Gallery</Link>
+                <Link to='/achievements'>Achievements</Link>
+                <Link to='/contact'>Contact</Link>
+            </div>
         </header>
     )
 }
