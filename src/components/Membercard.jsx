4ecdb566c10f1aeac5a0c9b5aa47e7e6fb0df04d
Your Name <you@example.com>
2022-06-01 09:24:43 +0200

    details added

--- src/components/Membercard.jsx
+++ src/components/Membercard.jsx
@@ -8,13 +8,13 @@ function Membercard({ info }) {
     return (
         <div className="cardcontainer">
             <div className="card">
-                <h3>{name}</h3>
-                <h5>{dob}</h5>
-                <h5>{height}</h5>
-                <h5>{weight}</h5>
-                <h5>{shoes}</h5>
-                <h5>{number}</h5>
-                <h5>{position}</h5>
+                <h3>Name: {name}</h3>
+                <h5>Date of Birth: {dob}</h5>
+                <h5>Height: {height}</h5>
+                <h5>Weight: {weight}</h5>
+                <h5>Shoes: {shoes}</h5>
+                <h5>Number: {number}</h5>
+                <h5>Position: {position}</h5>
             </div>
         </div>
     )
