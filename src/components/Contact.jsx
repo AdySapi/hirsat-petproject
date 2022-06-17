ab593a6324b731e5722b36dae848e263a2b8f46c
Your Name <you@example.com>
2022-06-17 10:08:43 +0200

    maps removed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Layout from './Layout';
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 function Contact() {
 
-    const { } = useLoadScript({ googleMapsApiKey: "" });
 
     return (
         <Layout>
             <div>Contact</div>
-
         </Layout>
     )
 }
