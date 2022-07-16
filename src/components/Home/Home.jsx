23a792370b54d061221c6481e2fd69a0465128bb
Your Name <you@example.com>
2022-07-16 14:27:57 +0200

    grid attempt

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <div className="homeContainer">
                 <div className="upcomingEvent">
                     <img></img>
-                    <span>Palermo here we come!</span>
+                    <h4 className="upcomingTitle">Palermo here we come!</h4>
                 </div>
                 <div className="playerOFTheYear">
                     <img src="https://instagram.fbud7-4.fna.fbcdn.net/v/t51.2885-15/273567397_2304414519696520_5248937601593635533_n.jpg?stp=c106.0.867.867a_dst-jpg_e15_s480x480&cb=2d435ae8-326fec31&_nc_ht=instagram.fbud7-4.fna.fbcdn.net&_nc_cat=104&_nc_ohc=svD5MQkvwioAX8rY1a_&edm=APU89FABAAAA&ccb=7-5&oh=00_AT_CaCgPUtAmbi0wuEkEU4C-05pdP5WrAiP4WDJjDpuObw&oe=62DA1D61&_nc_sid=86f79a" alt="" />
